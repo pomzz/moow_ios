4abf70fddcdf3419a280f520aa57bff6cb23561f
herashchenko.oleh <olehherashchenko@olehs-imac.local>
2016-04-07 12:57:39 -0700

    Initial Commit

---
+++ MoowLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoowLife
+//
+//  Created by Oleh Herashchenko on 4/7/16.
+//  Copyright © 2016 Oleh Herashchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
